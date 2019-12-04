60133d0d348f45eca8abdc888baad1a675ebd642
Jee Vang, Ph.D <vangjee@gmail.com>
2019-12-03 19:56:36 -0500

    refactored

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
+    function modifyMp3Links() {
+        const anchors = $('a[class="reference external"]').filter(function(index) { 
+            let href = $(this).attr('href');
+            return href.endsWith('mp3');
+        });
+        $(anchors).each(function(index, value) {
+            let href = $(this).attr('href');
+            $(this).click(function() {
+                console.log(href);
+                var sound = new Howl({
+                    src: [href]
+                })
+                sound.play();
+                return false;
+            });
+        });
+    }
+
     console.log('ready!');
     
-    const anchors = $('a[class="reference external"]').filter(function(index) { 
-        let href = $(this).attr('href');
-        return href.endsWith('mp3');
-     });
-     $(anchors).each(function(index, value) {
-         let href = $(this).attr('href');
-         $(this).click(function() {
-             console.log(href);
-             var sound = new Howl({
-                 src: [href]
-             })
-             sound.play();
-             return false;
-         });
-     });
-     
+    modifyMp3Links();
+    
     console.log('done!');
 });
